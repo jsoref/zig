978cb3ca67bf1830d14e3677224d5d350220a917
Josh Soref <2119212+jsoref@users.noreply.github.com>
2026-06-01 23:56:41 -0400

    spelling: intel's
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- src/link/SpirV/lower_invocation_globals.zig
+++ src/link/SpirV/lower_invocation_globals.zig
@@ -421,7 +421,7 @@ const ModuleBuilder = struct {
                 .OpTypeFunction => {
                     // Re-emitted in `emitFunctionTypes()`. We can do this because
                     // OpTypeFunction's may not currently be used anywhere that is not
-                    // directly with an OpFunction. For now we ignore Intels function
+                    // directly with an OpFunction. For now we ignore Intel's function
                     // pointers extension, that is not a problem with a generalized
                     // pass anyway.
                     continue;
